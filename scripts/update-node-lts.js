8e07c15bd516f723d8d3edeff850ee06a9653d8e
Cody Olsen <stipsan@gmail.com>
2023-06-07 03:06:17 +0200

    chore: move over presets from legacy

---
+++ scripts/update-node-lts.js
@@ -0,0 +1,11 @@
+import { readFile, writeFile } from 'node:fs/promises'
+import { version } from 'node:process'
+
+const url = new URL(`./node-lts.json`, import.meta.url)
+const config = JSON.parse(await readFile(url))
+
+const [major] = version.slice(1).split('.')
+
+config.packageRules[0].allowedVersions = `<=${major}`
+
+await writeFile(url, JSON.stringify(config))
