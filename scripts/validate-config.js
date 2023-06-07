0475476a8b1d4d9601b6ce29676f7b5239bc8ae1
Cody Olsen <stipsan@gmail.com>
2023-06-07 09:41:18 +0200

    chore: remove json-schema-remote

--- scripts/validate-config.js
+++ scripts/validate-config.js
@@ -2,26 +2,14 @@
 
 import 'zx/globals'
 
-import validator from 'json-schema-remote'
-
-const [schema, files] = await Promise.all([
-  spinner('fetching renovate-schema.json...', async () => {
-    const res = await fetch('https://docs.renovatebot.com/renovate-schema.json')
-    return res.json()
-  }),
-  glob(['./*.json', '.github/renovate.json'], {
-    ignore: ['package*.json'],
-  }),
-])
+const files = await glob(['./*.json', '.github/renovate.json'], {
+  ignore: ['package*.json'],
+})
 
 for (const file of files) {
   await spinner(`validating ${file}...`, async () => {
     try {
-      const jsonPromise = fs.readJson(file)
-      await Promise.all([
-        $`renovate-config-validator ${file}`,
-        validator.validate(await jsonPromise, schema),
-      ])
+      await $`renovate-config-validator ${file}`
     } catch (p) {
       throw new Error(
         `Validation failed for ${file}: ${p.stderr || p.stdout || p.message}`,
