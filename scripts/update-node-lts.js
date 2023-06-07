c7cce29d9717b4aba6fd35fc0584e0a45e706dac
Cody Olsen <stipsan@gmail.com>
2023-06-07 03:11:16 +0200

    chore: test maintenance script

--- scripts/update-node-lts.js
+++ scripts/update-node-lts.js
@@ -1,7 +1,7 @@
 import { readFile, writeFile } from 'node:fs/promises'
 import { version } from 'node:process'
 
-const url = new URL(`./node-lts.json`, import.meta.url)
+const url = new URL(`../node-lts.json`, import.meta.url)
 const config = JSON.parse(await readFile(url))
 
 const [major] = version.slice(1).split('.')
