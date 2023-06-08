f882ca75111b34843da4cbcae64dd5d5c6f95c26
Cody Olsen <stipsan@gmail.com>
2023-06-08 16:45:27 +0200

    chore: catch warnings during tests

--- scripts/validate-config.js
+++ scripts/validate-config.js
@@ -9,7 +9,11 @@ const files = await glob(['./*.json', '.github/renovate.json'], {
 for (const file of files) {
   await spinner(`validating ${file}...`, async () => {
     try {
-      console.log((await $`renovate-config-validator ${file}`).stdout)
+      const result = (await $`renovate-config-validator ${file}`).stdout
+      console.log(result)
+      if (result.includes('WARN')) {
+        throw new Error(`Warnings found in config: ${file}`)
+      }
     } catch (p) {
       throw new Error(
         `Validation failed for ${file}: ${p.stderr || p.stdout || p.message}`,
