93c67c13b45bfac870546294d09318ac625a38e5
Cody Olsen <stipsan@gmail.com>
2024-04-10 22:23:48 +0200

    chore: fix path

--- scripts/update-typescript.js
+++ scripts/update-typescript.js
@@ -1,7 +1,7 @@
 import { readFile, writeFile } from 'node:fs/promises'
 import pkg from '@sanity/tsdoc/package.json' assert {type: 'json'}
 
-const {typescript: version} = pkg.devDependencies
+const {typescript: version} = pkg.dependencies
 
 
 const url = new URL(`../typescript.json`, import.meta.url)
