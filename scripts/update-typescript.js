807cc9988663a4f01f759d23f67fb1cc2581f9f2
Cody Olsen <stipsan@gmail.com>
2025-07-31 21:34:58 +0200

    chore: fix update script

--- scripts/update-typescript.js
+++ scripts/update-typescript.js
@@ -1,7 +1,7 @@
 import { readFile, writeFile } from 'node:fs/promises'
-import pkg from '@sanity/tsdoc/package.json' assert { type: 'json' }
+import pkg from '@sanity/tsdoc/package.json' with { type: 'json' }
 
-const { typescript: version } = pkg.dependencies
+const { typescript: version } = pkg.devDependencies
 
 const url = new URL(`../typescript.json`, import.meta.url)
 const config = JSON.parse(await readFile(url))
